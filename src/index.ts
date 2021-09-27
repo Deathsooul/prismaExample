3f05947744e401cc596a4dafd41550f065185fac
Deathsooul <wbescrovaine@gmail.com>
2021-09-26 22:59:42 -0300

    Prisma studio

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,25 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-prisma.user.create({
-  data: {
-    id: "user-1",
-    name: "Wallace Bescrovaine",
-    email: "wbescrovaine@gmail.com",
-    groups: {
-      connectOrCreate: {
-        where: {
-          id: "group-1",
-        },
-        create: {
-          id: "group-1",
-          titles: "Group 01"
+async function main() {
+  await prisma.user.create({
+    data: {
+      id: "user-1",
+      name: "Wallace Bescrovaine",
+      email: "wbescrovaine@gmail.com",
+      groups: {
+        connectOrCreate: {
+          where: {
+            id: "group-1",
+          },
+          create: {
+            id: "group-1",
+            titles: "Group 01",
+          },
         },
       },
     },
-  },
-});
+  });
+}
+
+main()
